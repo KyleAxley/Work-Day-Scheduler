5a62df86d326bc1bea8b8be01de7b2d2f13f1b26
Your Name <you@example.com>
2022-06-17 19:03:16 -0400

    forgot to add current day display to the top

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $('#currentDay').text(moment().format("MMMM Do YYYY, h:mm:ss a"));
     $('.saveBtn').on('click',function(){
         //store in localstorage
         var value = $(this).siblings('.description').val()
